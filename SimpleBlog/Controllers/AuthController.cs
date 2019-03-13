819e4befe6bab6b0887ed665d3821b93dfb777c4
Oğuzhan GÖKÇE <37300960+oguzhangokce@users.noreply.github.com>
2019-03-13 17:20:21 +0300

    Add files via upload

---
+++ SimpleBlog/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using SimpleBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+           if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+           if (form.UserName != "username")
+            {
+                ModelState.AddModelError("UserName", "User doesn't exist.");
+
+            }
+            return Content("The form is valid");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AuthRegister form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            
+            return Content(String.Format("Kayıt baraşıyla alındı.<br /><br />Öğrenci NO: {0}<br />Öğrenci Adı : {1} {2}<br />Email: {3}", form.StudentNumber, form.StudentName, form.StudentSurname, form.StudentEmail));
+        }
+    }
+}
---
+++ SimpleBlog/ViewModels/Auth.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.ViewModels
+{
+    public class AuthLogin
+
+    {
+        public string Test { get; set; }
+        
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(16)]
+        public string UserName { get; set; }
+        [Required]
+        [MinLength(6)]
+        [MaxLength(16)]
+        public string Password { get; set; }
+    }
+
+    public class AuthRegister
+    {
+        [Required]
+        [MinLength(10)]
+        [MaxLength(10)]
+        public int StudentNumber { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string StudentName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public string StudentSurname { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string StudentEmail { get; set; }
+    }
+}
